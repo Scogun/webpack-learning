d6a0005f22d4be17e5ee3d66e5f561239bd8c3a4
Sergey <AntonovSergeyS@gmail.com>
2023-10-19 17:41:46 +0200

    Add CSS loader

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,11 @@ kotlin {
     }
     sourceSets {
         val jsMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions:1.0.1-pre.634")
+                implementation(devNpm("css-loader", "6.8.1"))
+                implementation(devNpm("style-loader", "3.3.3"))
+            }
         }
     }
 }
--- src/jsMain/kotlin/org/example/Main.kt
+++ src/jsMain/kotlin/org/example/Main.kt
@@ -1,9 +1,12 @@
 package org.example
 
+import kotlinext.js.require
 import org.example.services.AlertService
 import org.example.services.ComponentService
 
 fun main() {
+
+    require<Any>("./main.css")
 
     val alertService = AlertService()
     val componentService = ComponentService()
