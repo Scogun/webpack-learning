c98d645ab47ef43601fd8ec0ba38ee76334dc01c
Sergey <AntonovSergeyS@gmail.com>
2023-10-20 17:16:41 +0200

    Switch on Sass

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,18 +26,9 @@ kotlin {
                 implementation(devNpm("sass-loader", "13.3.2"))
                 implementation(devNpm("bootstrap", "5.3.2"))
                 implementation(devNpm("@popperjs/core", "2.11.8"))
-                implementation(devNpm("node-sass", "8.0.0"))
+                implementation(devNpm("sass", "1.69.4"))
                 implementation(devNpm("html-webpack-plugin", "5.5.3"))
             }
-        }
-    }
-}
-
-tasks.withType<KotlinNpmInstallTask> {
-    doLast {
-        copy {
-            from("node-sass")
-            into("${layout.buildDirectory.get()}\\js\\node_modules\\node-sass\\vendor")
         }
     }
 }
